6319323d98ad829d617042b3865cf9a5bbfb02d9
Nikolett Palyi <nikpalyi@gmail.com>
2020-01-30 15:25:17 +0000

    Updates

---
+++ precache-manifest.e6fb2814b8490248d08ccfc66c562022.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4666bd3ed15dbb97b3c80564e77564dd",
+    "url": "/color-picker-react/index.html"
+  },
+  {
+    "revision": "5c82ff53758bba3c5ba2",
+    "url": "/color-picker-react/static/css/main.711b8760.chunk.css"
+  },
+  {
+    "revision": "535b10d8fc79b4453755",
+    "url": "/color-picker-react/static/js/2.fe442e9a.chunk.js"
+  },
+  {
+    "revision": "5c82ff53758bba3c5ba2",
+    "url": "/color-picker-react/static/js/main.614bd9ea.chunk.js"
+  },
+  {
+    "revision": "74dea7e948bfbfe1fc9f",
+    "url": "/color-picker-react/static/js/runtime-main.ae2c8b86.js"
+  }
+]);
